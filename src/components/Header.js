097358a69a35435e6893fd957d8b68c2d44b7367
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2025-03-23 16:23:18 +0800

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => (
+  <header>
+    <div className="header-content">
+      <h1>Word Spinner</h1>
+      <p>Spin, Blend, and Shine! 🌟</p>
+    </div>
+  </header>
+);
+
+export default Header;
