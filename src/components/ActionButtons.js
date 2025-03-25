3efef826ed5b5b364baef67fec735eaea2c3f967
JastonChamp <142294191+JastonChamp@users.noreply.github.com>
2025-03-25 17:25:49 +0800

    Update ActionButtons.js

--- src/components/ActionButtons.js
+++ src/components/ActionButtons.js
@@ -3,27 +3,27 @@ import { GameContext } from '../context/GameContext';
 import '../styles/ActionButtons.css';
 
 const ActionButtons = () => {
-  const { state, dispatch } = useContext(GameContext);
+ const { state, dispatch } = useContext(GameContext);
 
-  const handleSpin = () => {
-    dispatch({ type: 'SPIN_WORD' });
-  };
+ const handleSpin = () => {
+ dispatch({ type: 'SPIN_WORD' });
+ };
 
-  const handleRepeat = () => {
-    dispatch({ type: 'REPEAT_WORD' });
-  };
+ const handleRepeat = () => {
+ dispatch({ type: 'REPEAT_WORD' });
+ };
 
-  const handleHint = () => {
-    dispatch({ type: 'SHOW_HINT' });
-  };
+ const handleHint = () => {
+ dispatch({ type: 'SHOW_HINT' });
+ };
 
-  return (
-    <div className="action-buttons">
-      <button onClick={handleSpin}>Spin</button>
-      <button onClick={handleRepeat}>Repeat</button>
-      <button onClick={handleHint}>Hint</button>
-    </div>
-  );
+ return (
+ <div className="action-buttons">
+ <button onClick={handleSpin}>Spin</button>
+ <button onClick={handleRepeat}>Repeat</button>
+ <button onClick={handleHint}>Hint</button>
+ </div>
+ );
 };
 
 export default ActionButtons;
